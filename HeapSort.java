56d1f60a9cc428e33e68b08e08b06ee9489f2055
niuqinghua <nqh_2005@163.com>
2014-05-30 19:48:21 +0800

    Create HeapSort.java

---
+++ HeapSort.java
@@ -0,0 +1,46 @@
+package test.sort;
+
+import java.util.Arrays;
+
+public class HeapSort {
+
+	public static void main(String[] args) {
+		int[] data = {49, 38, 65, 97, 76, 13, 27, 49};
+		System.out.println(Arrays.toString(data));
+		sort(data);
+		System.out.println(Arrays.toString(data));
+	}
+	
+	public static void sort(int[] data) {
+		int length = data.length;
+		for(int i = (length -1) / 2; i >= 0; i--) {
+			adjust(data, i, length);
+		}
+		int count = length - 1;
+		while(count > 0) {
+			swap(data, 0, count);
+			count--;
+			adjust(data, 0, count + 1);
+		}
+	}
+	
+	public static void adjust(int[] data, int parentIdx, int length) {
+		for(int j = (parentIdx + 1) * 2 - 1; j < length; j = (j + 1) * 2 - 1) {
+			if(j + 1 <= length - 1 && data[j] > data[j+1]) {
+				j++;
+			}
+			if(data[parentIdx] <= data[j]) {
+				break;
+			}
+			swap(data, parentIdx, j);
+			parentIdx = j;
+		}
+	}
+	
+   private static void swap(int[] a,int i,int j){    
+	   int temp = a[i];
+        a[i] = a[j];    
+        a[j] = temp;    
+    }
+   
+}
